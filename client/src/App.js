1976df4b77acfd386e3a9344c1529549fe0d9007
Emily Fernschild <emilyfernschild@gmail.com>
2023-02-24 12:01:02 -0500

    configured client-side routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
+// client/src/components/App.js
 import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,10 +12,13 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <h1>Page Count: {count}</h1>
-    </div>
-  );
+      <div className="App">
+        <Routes>
+          <Route path="/testing" element= {<h1>Test Route</h1>} />
+          <Route path="/" element={<h1>Page Count: {count}</h1>} />
+        </Routes>
+      </div>
+  )
 }
 
 export default App;
