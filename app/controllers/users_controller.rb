742e4a41fe5b9d8b5f95c18e081b58bb00f9daeb
Emily Fernschild <emilyfernschild@gmail.com>
2023-03-16 13:11:11 -0400

    backend autologin feat

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,38 +8,36 @@ class UsersController < ApplicationController
 
   # GET /users/1
   def show
-    render json: @user
+    render json: set_user, status: :ok
+  end
+
+  # for autologin feat
+  def user 
+    render json: @current_user, status: :ok
   end
 
   # POST /users
   def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
+    user = User.create!(user_params)
+    session[:user_id] = user.id 
+    render json: user, status: :created
   end
 
   # PATCH/PUT /users/1
   def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
+    render json: set_user.update!(user_params), status: :accepted  
   end
 
   # DELETE /users/1
   def destroy
-    @user.destroy
+    set_user.destroy
+    head.no_content
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      User.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,14 @@ Rails.application.routes.draw do
   resources :comments
   resources :jeans
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  post "/signup", to: "users#create"
+  get "/user", to: "users#user"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 
   get '*path',
       to: 'fallback#index',
       constraints: ->(req) { !req.xhr? && req.format.html? }
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
 end
